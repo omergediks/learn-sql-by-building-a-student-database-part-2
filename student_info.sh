f9951ff86a58527ec6a0935ba590e43ce9814972
Ömer Gedik <omergedikweb@gmail.com>
2024-03-13 04:22:40 +0000

    Save progress: 1770.1

--- student_info.sh
+++ student_info.sh
@@ -21,6 +21,5 @@ echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE last_name >
 # Print the next sentence
 echo -e "\n$(echo "Last name of students whose last name contains a case insensitive 'sa' or have an 'r' as the second to last letter:")"
 
-# Query the database for courses meeting the specified conditions
-echo "$($PSQL "SELECT last_name FROM students WHERE last_name ILIKE '%sa%' OR last_name LIKE '%r_'")"
-echo -e "\nFirst name, last name, and GPA of students who have not selected a major and either their first name begins with 'D' or they have a GPA greater than 3.0:"
+
+ echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE major_id IS NULL AND (first_name LIKE 'D%' OR gpa > 3.0)")"
