a7efd00df2e6e3a7d791a626611c4e33fbebe58f
Ömer Gedik <omergedikweb@gmail.com>
2024-03-13 03:42:02 +0000

    Save progress: 1380.1

--- student_info.sh
+++ student_info.sh
@@ -7,3 +7,6 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 
 
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
+
+# Print what the above sentence asks for
+echo -e "\n$(echo "All course names whose first letter is before 'D' in the alphabet:")"
