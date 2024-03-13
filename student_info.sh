185ef29fb6d7b523359594f61d29cbaf7c2569b5
Ömer Gedik <omergedikweb@gmail.com>
2024-03-13 03:31:53 +0000

    Save progress: 1280.1

--- student_info.sh
+++ student_info.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Info about my computer science students from students database
-echo -e "\n~~ My Computer Science Students ~~\n"
+
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 echo -e "\n~~ My Computer Science Students ~~\n"
 
 # Query the database for student information
 $PSQL "SELECT * FROM students;"
+
+# Print the header for students with a 4.0 GPA
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
